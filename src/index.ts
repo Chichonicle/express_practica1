0f327436688b32f4f33ac8ab9092854585623007
Christian <christianperez.rbb@gmail.com>
2023-10-18 13:40:50 +0200

    feat: Create app get, put, post and delete.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,37 @@ import express from "express";
 
 const app = express()
 
+app.get('/films', (req, res) => {
+    // logica de lo que quiero hacer/devolver
+
+    return res.send('GET FILMS')
+})
+
+app.post('/films', (req, res) => {
+    // logica para crear peliculas
+    console.log('create');
+
+    return res.send('CREATE FILM')
+})
+
+app.put('/films/:id', (req, res) => {
+    // logica para actualizar peliculas
+    // console.log(req.params.id);
+
+    const filmId = req.params.id
+    
+
+    return res.send('UPDATE FILM' + filmId)
+})
+
+app.delete('/films/:id', (req, res) => {
+    // logica para eliminar peliculas
+
+    const filmId = req.params.id
+
+    return res.send('DELETE FILM' + filmId)
+})
+
 app.listen(4000, () => {
     console.log(`Server running`);
 })
